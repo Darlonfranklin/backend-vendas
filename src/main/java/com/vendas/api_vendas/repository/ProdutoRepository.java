678edb00c4fd81b48befa9ec942a3a70b0fc371a
FernandaHallmann <fernanda.hallmann@gmail.com>
2024-11-05 19:01:35 -0300

    Atualizado

--- src/main/java/com/vendas/api_vendas/repository/ProdutoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.vendas.api_vendas.repository;
-
-import com.vendas.api_vendas.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoService extends JpaRepository<Produto, Long> { }
--- src/main/java/com/vendas/api_vendas/service/ProdutoService.java
+++
@@ -1,4 +0,0 @@
-package com.vendas.api_vendas.service;
-
-public class Produto {
-}
